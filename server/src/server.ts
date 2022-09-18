2d08eec88c6a63c560a23e903ab795aef14b5045
Otávio Augusto <otavioaugustobrb19@gmail.com>
2022-09-18 12:13:49 -0300

    backend finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client' 
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 const prisma = new PrismaClient()
 
@@ -20,8 +27,24 @@ app.get('/games', async (request, response) => {
     return response.json(games)
 })
 
-app.post('/ads', (request, response) => {
-    return response.status(201).json([]);
+app.post('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+    const body: any = request.body;
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name, 
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return response.status(201).json(ad);
 })
 
 app.get('/games/:id/ads', async (request, response) => {
@@ -51,7 +74,9 @@ app.get('/games/:id/ads', async (request, response) => {
     return response.json(ads.map(ad => {
         return {
             ...ad,
-            weekDays: ad.weekDays.split(',')
+            weekDays: ad.weekDays.split(', '),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd)
         }
     }))
 })
